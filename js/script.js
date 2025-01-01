f500bd824aba0feafcb85bf31c2782cb6f8e5ec7
s-khodjaev <skhodjaev@icloud.com>
2025-01-01 22:37:10 +0500

    add navbar, header and replaceText script

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Replace Text In Header
+const checkReplace = document.querySelector('.replace-me');
+
+if (checkReplace !== null) {
+  const replace = new ReplaceMe(checkReplace, {
+    animation: 'animated fadeIn',
+    speed: 2000,
+    separator: ',',
+    loopCount: 'infinite',
+    autoRun: true,
+  });
+}
